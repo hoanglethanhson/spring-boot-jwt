33f9a6e47df28422e2e3b48396fedf41e65e902c
Son Le <leson1998ls@gmail.com>
2020-11-18 00:55:40 +0700

    first commit

---
+++ src/test/java/com/leson/demojwt/DemoJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leson.demojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
