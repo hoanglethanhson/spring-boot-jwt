999032bd201b46d4a49841d57ecb68da32f5cab4
Son Le <leson1998ls@gmail.com>
2020-11-19 00:30:26 +0700

    complete example

---
+++ src/main/java/com/leson/demojwt/filter/JWTLoginFilter.java
@@ -0,0 +1,44 @@
+package com.leson.demojwt.filter;
+
+import com.leson.demojwt.service.TokenAuthenticationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+@Slf4j
+public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
+    public JWTLoginFilter(String url, AuthenticationManager authManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        String username = httpServletRequest.getParameter("username");
+        String password = httpServletRequest.getParameter("password");
+        log.info("JWTLoginFilter attemptAuthentication: username/password: {}/{}", username, password);
+        return getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(username, password, Collections.emptyList()));
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        log.info("JWTLoginFilter.successfulAuthentication");
+
+        //write authorization to header of response
+        TokenAuthenticationService.addAuthentication(response, authResult.getName());
+
+        String authorizationString = response.getHeader("Authorization");
+        log.info("Authorization string: {}", authorizationString);
+    }
+}
